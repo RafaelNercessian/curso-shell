fed06eaa2706de35e3a04a7811129703ea2a3e76
RafaelNercessian <rafanercessian@gmail.com>
2017-07-17 10:19:42 -0300

    Criando diretorio png

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 cd ~/Downloads/imagens-livros
+if [ ! -d png ]
+then
+	mkdir png
+fi
+
+
 for imagem in *.jpg
 do
-	convert $imagem $imagem.png
+	imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
 
