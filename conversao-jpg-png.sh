10fb48b1b5d0a5104a30882d1a0a4afeaf9f7557
RafaelNercessian <rafanercessian@gmail.com>
2017-07-17 09:01:03 -0300

    Laco for

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -2,5 +2,8 @@
 
 CAMINHO_IMAGENS=~/Downloads/imagens-livros
 
-convert $CAMINHO_IMAGENS/$1.jpg $CAMINHO_IMAGENS/$1.png
-convert $CAMINHO_IMAGENS/$2.jpg $CAMINHO_IMAGENS/$2.png
+for imagem in $@
+do
+	convert $CAMINHO_IMAGENS/$imagem.jpg $CAMINHO_IMAGENS/$imagem.png
+done
+
