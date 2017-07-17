818e69be8a6b7407ccfc020347031c523f4ec1c0
RafaelNercessian <rafanercessian@gmail.com>
2017-07-17 12:51:03 -0300

    Verificando status de saida

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ~/Downloads/imagens-livros
+converte_imagem(){
+cd ~/Downloads/IMAGENS-LIVROS
 if [ ! -d png ]
 then
 	mkdir png
@@ -9,7 +10,15 @@ fi
 
 for imagem in *.jpg
 do
-	imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+	local imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
 	convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
 done
+}
 
+converte_imagem 2>erros_conversao.txt
+if [ $? -eq 0 ]
+then
+	echo "Conversao realizada com sucesso"
+else
+	echo "Houve uma falha no processo"
+fi
