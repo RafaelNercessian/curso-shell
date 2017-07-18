a229e1c75f053c751b1c563c9dc30967cf1bcdb6
RafaelNercessian <rafanercessian@gmail.com>
2017-07-18 09:54:51 -0300

    Recursao 2

--- conversao-novos-livros.sh
+++ conversao-novos-livros.sh
@@ -1,21 +1,30 @@
+
 #!/bin/bash
 
-cd ~/Downloads/imagens-novos-livros
-for arquivo in *
-do
-	if [ -d $arquivo ]
-	then
-		cd $arquivo
-		for conteudo_arquivo in *
-		do
-			if [ -d $conteudo_arquivo ]
-			then
-				#Entrar no diretorio e fazer a varredura
-			else
-				#Conversao jpg para png
-			fi
-		done
-	else
-		#Conversao jpg para png
-	fi
-done
+converte_imagem(){
+	local caminho_imagem=$1
+	local imagem_sem_extensao=$(ls $caminho_imagem | awk -F. '{ print $1 }')
+	convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+}
+
+varrer_diretorio(){
+	cd $1
+	for arquivo in *
+	do
+		caminho_arquivo=$(find ~/Downloads/imagens-novos-livros -name $arquivo)
+		if [ -d $caminho_arquivo ]
+		then
+			varrer_diretorio $caminho_arquivo
+		else
+			converte_imagem $caminho_arquivo
+		fi
+	done
+}
+
+varrer_diretorio ~/Downloads/imagens-novos-livros
+if [ $? -eq 0 ]
+then
+	echo "Conversao realizada com sucesso"
+else
+	echo "Houve um problema na conversao"
+fi
