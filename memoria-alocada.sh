c529d949944e946af351a36051a62c8207c26954
RafaelNercessian <rafanercessian@gmail.com>
2017-07-18 12:54:34 -0300

    Criando arquivos

--- memoria-alocada.sh
+++ memoria-alocada.sh
@@ -3,5 +3,6 @@
 processos=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
 for pid in $processos
 do
-	echo $(ps -p $pid -o comm=)
+	nome_processo=$(ps -p $pid -o comm=)
+	echo -n $(date +%F,%H:%M:%S,) >> $nome_processo.log
 done
