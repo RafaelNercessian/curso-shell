a262adfdcc9807865226292a3e39de143605df82
RafaelNercessian <rafanercessian@gmail.com>
2017-07-19 16:19:52 -0300

    Monitoracao crontab

--- monitoracao-servidor.sh
+++ monitoracao-servidor.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 resposta_http=$(curl --write-out %{http_code} --silent --output /dev/null  http://localhost)
-
-echo $resposta_http
-
+if [ $resposta_http -eq 200 ]
+then
+	echo "Tudo esta ok com o servidor"
+else
+	echo "Houve um problema no servidor. Tentando reinicializar"
+	systemctl restart apache2
+	echo "...Servidor reinicializado"
+fi
