03397b24273a30e862cb96e6d0f75da0a11b4e54
RafaelNercessian <rafanercessian@gmail.com>
2017-07-20 09:29:42 -0300

    Mandando email para o usuario

--- monitoracao-servidor.sh
+++ monitoracao-servidor.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 resposta_http=$(curl --write-out %{http_code} --silent --output /dev/null  http://localhost)
-if [ $resposta_http -eq 200 ]
+if [ $resposta_http -ne 200 ]
 then
-	echo "Tudo esta ok com o servidor"
-else
-	echo "Houve um problema no servidor. Tentando reinicializar"
+mail -s "Problema no servidor" adm.mutillidae@gmail.com<<del
+Houve um problema no servidor e os usuarios pararam de ter acesso ao conteudo web.
+del
 	systemctl restart apache2
-	echo "...Servidor reinicializado"
 fi
