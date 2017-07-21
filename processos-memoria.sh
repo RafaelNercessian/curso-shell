400f0e466191fa3fd08a5418dd6701932fb2d81f
RafaelNercessian <rafanercessian@gmail.com>
2017-07-21 18:15:23 -0300

    Finalizando processos-memoria

--- processos-memoria.sh
+++ processos-memoria.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+if [ ! -d log ]
+then
+	mkdir log
+fi
+
+
+processos_memoria(){
 processos=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
 for pid in $processos
 do
-	nome_processo=$ (ps -p $pid -o comm=)
+	nome_processo=$(ps -p $pid -o comm=)
 	echo -n $(date +%F,%H:%M:%S,) >> log/$nome_processo.log
+	tamanho_processo=$(ps -p $pid -o size | grep [0-9])
+	echo "$(bc <<< "scale=2;$tamanho_processo/1024") MB" >> log/$nome_processo.log
 done
+}
 
+processos_memoria
+if [ $? -eq 0 ]
+then
+	echo "Os arquivos foram salvos com sucesso"
+else
+	echo "Houve um problema na hora de salvar os arquivos"
+fi
