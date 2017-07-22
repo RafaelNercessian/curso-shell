d944b113c3b565843600bbdbac7e8b3cdcc368da
RafaelNercessian <rafanercessian@gmail.com>
2017-07-22 00:20:33 -0300

    Guardando processo em uma variavel

--- processos-memoria.sh
+++ processos-memoria.sh
@@ -3,6 +3,7 @@
 processos=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
 for pid in $processos
 do
-	echo $(ps -p $pid -o comm=)
+	nome_processo=$ (ps -p $pid -o comm=)
+	echo -n $(date +%F,%H:%M:%S,) >> log/$nome_processo.log
 done
 
